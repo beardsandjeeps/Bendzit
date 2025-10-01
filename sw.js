1d1f7ef70ade95676938bd131293ef8d1c6328c5
beardsandjeeps <beardsandjeeps@gmail.com>
2025-10-01 15:58:26 -0400

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,1 @@
-const CACHE='tube-bend-liteplus-v3';
-const ASSETS=['./','./index.html','./script.js','./manifest.webmanifest','./icons/icon-192.png','./icons/icon-512.png','https://cdn.jsdelivr.net/npm/jspdf@2.5.1/dist/jspdf.umd.min.js'];
-self.addEventListener('install',e=>{e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)))});
-self.addEventListener('fetch',e=>{e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request)))});
+self.addEventListener('fetch', e=>{});
